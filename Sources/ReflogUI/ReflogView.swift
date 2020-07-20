626c15f2c2500bcbb66db92551f699328e0cc8a1
Daniel Tull <dt@danieltull.co.uk>
2020-07-21 00:09:34 +0100

    Style the reflog view

--- Sources/ReflogUI/ReflogView.swift
+++ Sources/ReflogUI/ReflogView.swift
@@ -3,6 +3,13 @@ import Foundation
 import GitKit
 import SwiftUI
 
+let dateFormatter: DateFormatter = {
+    let df = DateFormatter()
+    df.timeStyle = .short
+    df.dateStyle = .long
+    return df
+}()
+
 struct ReflogView: View {
 
     let repository: Repository
@@ -24,45 +31,97 @@ struct ItemView: View {
 
     var body: some View {
         HStack {
-            Text(item.new.description.dropLast(31))
-            Text(item.message)
+            ObjectIDView(item: item)
+            VStack(alignment: .leading) {
+                Text(dateFormatter.string(from: item.committer.date))
+                Text(item.message.dropFirst(item.kind.trimAmount))
+            }
+            .padding(5)
+            .font(.system(size: 17, weight: .regular, design: .default))
             Spacer(minLength: 0)
         }
-        .padding(10)
-        .background(RoundedRectangle(cornerRadius: 5).fill(item.backgroundColor))
-        .foregroundColor(item.foregroundColor)
+        .background(RoundedRectangle(cornerRadius: 5).stroke(lineWidth: 4).foregroundColor(item.kind.backgroundColor))
+        .clipShape(RoundedRectangle(cornerRadius: 5))
+        .lineLimit(1)
+    }
+}
+
+struct ObjectIDView: View {
+
+    let item: Reflog.Item
+
+    var body: some View {
+        VStack(alignment: .leading) {
+            Text(item.kind.rawValue)
+            Text(item.new.description.dropLast(40 - Reflog.Item.Kind.maxLength))
+        }
         .font(.system(size: 17, weight: .regular, design: .monospaced))
+        .padding(10)
+        .frame(maxHeight: .infinity)
+        .background(item.kind.backgroundColor)
+        .foregroundColor(item.kind.foregroundColor)
         .lineLimit(1)
     }
 }
 
 extension Reflog.Item {
 
-    private static let actions: [(name: String, foreground: Color, background: Color)] = [
-        ("pull", .white, .pink),
-        ("commit", .black, .green),
-        ("checkout", .black, .blue),
-        ("reset", .white, .red),
-        ("rebase", .white, .purple),
-        ("cherry-pick", .white, .orange),
-        ("merge", .black, .yellow),
-    ]
+    enum Kind: String, CaseIterable {
+        case pull = "pull"
+        case commit = "commit"
+        case checkout = "checkout"
+        case reset = "reset"
+        case rebase = "rebase"
+        case cherrypick = "cherry-pick"
+        case merge = "merge"
+        case unknown = ""
 
-    var backgroundColor: Color {
+        static var maxLength: Int { allCases.map(\.rawValue).map(\.count).reduce(0, max) }
 
-        for action in Reflog.Item.actions {
-            if message.hasPrefix(action.name) { return action.background }
+        var foregroundColor: Color {
+            switch self {
+            case .pull: return .white
+            case .commit: return .black
+            case .checkout: return .black
+            case .reset: return .white
+            case .rebase: return .white
+            case .cherrypick: return .white
+            case .merge: return .black
+            case .unknown: return .white
+            }
         }
 
-        return .gray
+        var backgroundColor: Color {
+            switch self {
+            case .pull: return .pink
+            case .commit: return .green
+            case .checkout: return .blue
+            case .reset: return .red
+            case .rebase: return .purple
+            case .cherrypick: return .orange
+            case .merge: return .yellow
+            case .unknown: return .gray
+            }
+        }
+
+        var trimAmount: Int {
+            switch self {
+            case .pull: return 5
+            case .commit: return 8
+            case .checkout: return 10
+            case .reset: return 7
+            case .rebase: return 7
+            case .cherrypick: return 13
+            case .merge: return 6
+            case .unknown: return 0
+            }
+        }
     }
 
-    var foregroundColor: Color {
-
-        for action in Reflog.Item.actions {
-            if message.hasPrefix(action.name) { return action.foreground }
+    var kind: Kind {
+        for kind in Kind.allCases {
+            if message.hasPrefix(kind.rawValue) { return kind }
         }
-
-        return .white
+        return .unknown
     }
 }
